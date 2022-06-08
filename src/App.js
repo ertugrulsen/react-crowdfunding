d70d9ff3c2ef71d004efc0f00bcf917742e2297a
ErtugrulSen <senertugrul91@gmail.com>
2022-06-08 23:53:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
+import { Container, Menu } from 'semantic-ui-react';
 import './App.css';
+import { Campaign } from './components/Campaign';
+import { Home } from './components/Home';
+import { NotFound } from './components/NotFound';
+import history from './history';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router history={history}>
+        <Container>
+
+          <Menu secondary>
+            <Menu.Item
+              name='home'
+              onClick={this.navigateToHome}
+            />
+          </Menu>
+
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/campaigns/:address' component={Campaign} />
+            <Route component={NotFound} />
+          </Switch>
+
+        </Container>
+      </Router>
+    );
+  }
+
+
+  navigateToHome(e){
+    e.preventDefault();
+    history.push('/');
+  }
 }
 
 export default App;
